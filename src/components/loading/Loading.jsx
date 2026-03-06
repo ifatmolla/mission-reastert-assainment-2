d6555b51287dd29b24701ba09ac9c81fda8966a1
Ifat Molla <ifatmolla812@gmail.com>
2026-03-06 17:29:57 +0700

    alart show but use react-toastify

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -4,6 +4,7 @@ const Loading = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-white">
             
+            
             <div className="w-12 h-12 border-4 border-purple-400 border-t-transparent rounded-full animate-spin"></div>
         </div>
     );
